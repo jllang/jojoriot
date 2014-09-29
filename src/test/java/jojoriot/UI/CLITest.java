8844f680f260cada5ac8c2c30460dd20e9a8b4b5
Riku Walve <riku.walve@gmail.com>
2014-09-29 12:09:12 +0000

    pieni testien päivitys

--- src/test/java/jojoriot/UI/CLITest.java
+++ src/test/java/jojoriot/UI/CLITest.java
@@ -1,19 +1,16 @@
-package jojoriot.viitemanageri;
+package jojoriot.UI;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
 import jojoriot.IO.*;
-import jojoriot.UI.*;
 import jojoriot.references.*;
 import jojoriot.viitemanageri.*;
 
-/**
- *
- * @author John Lång <jllang@cs.helsinki.fi>
- */
-public class MainTest {
+public class CLITest {
     private Stub io;
 
     private void start(String... inputs) {
@@ -39,20 +36,29 @@ public class MainTest {
     @Test
     public void previewPrintsCorrectly() {
         start("1", "asd", "asd", "asd", "2014", "", "", "", "", "", "", "2", "3");
-        assertTrue(io.getPrints().contains("author: asd\n"));
-        assertTrue(io.getPrints().contains("title: asd\n"));
-        assertTrue(io.getPrints().contains("journal: asd\n"));
-        assertTrue(io.getPrints().contains("year: 2014\n"));
+
+        ArrayList<String> prints = io.getPrints();
+        assertTrue(prints.contains("author: asd\n"));
+        assertTrue(prints.contains("title: asd\n"));
+        assertTrue(prints.contains("journal: asd\n"));
+        assertTrue(prints.contains("year: 2014\n"));
     }
 
     @Test
-    public void krapulasaatana() {
-        Article asd = new Article("asd", "asd", "asd", "2014", "asd", "asd", "asd", "asd", "asd", "asd");
-        asd.delete("volume");
-        asd.get("title");
+    public void nonNumberCommandFails() {
+        start("a", "3");
+        assertTrue(io.getPrints().contains("Please input a number."));
+    }
 
-        Session s = new Session();
-        s.preview();
+    @Test
+    public void numericNonCommandFails() {
+        start("4", "3");
+        assertTrue(io.getPrints().contains("Unknown command."));
+    }
+
+    @Test
+    public void programExitsNicely() {
+        start("3");
+        assertTrue(io.getPrints().contains("Thank you for using Viitemanageri!"));
     }
 }
-
