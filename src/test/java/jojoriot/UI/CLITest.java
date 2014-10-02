0cf95c1e91a76cb1288aa1615bd7125c16511536
Otto A Vuorinen <oavuorin@cs.helsinki.fi>
2014-10-02 20:02:23 +0300

    CLITest coverage >90%

--- src/test/java/jojoriot/UI/CLITest.java
+++ src/test/java/jojoriot/UI/CLITest.java
@@ -2,9 +2,12 @@ package jojoriot.UI;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
+import jojoriot.references.Article;
 import jojoriot.viitemanageri.Session;
 import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -21,13 +24,14 @@ public class CLITest {
     public CLITest() {
     }
 
-    @BeforeClass
-    public static void setupClass() {
-        out = new ByteArrayOutputStream();
-        // virheulostulo = new ByteArrayOutputStream();
-    }
+//    @BeforeClass
+//    public static void setupClass() {
+//        out = new ByteArrayOutputStream();
+//        // virheulostulo = new ByteArrayOutputStream();
+//    }
 
     public static void setupTest(final String input, final Session session) {
+        out = new ByteArrayOutputStream();
         cli = new CLI(new Scanner(input), new PrintStream(out), session);
     }
 
@@ -48,6 +52,22 @@ public class CLITest {
         System.setOut(null);
         // System.setErr(null);
     }
+    
+    private Article getTestArticle() {
+        LinkedHashMap<String, String> fields = new LinkedHashMap<>();
+        fields.put("author", "Esko");
+        fields.put("title", "Juttu");
+        fields.put("journal", "Lehti");
+        fields.put("year", "1666");
+        Article article = new Article("esimerkki", fields);
+        return article;
+    }
+    
+    private Session getSessionWithArticle() {
+        Session session = new Session();
+        session.add(getTestArticle());
+        return session;
+    }
 
     /**
      * Tests that the exiting line is on the correct line of the output.
@@ -61,5 +81,61 @@ public class CLITest {
         assertEquals("> Thank you for using Viitemanageri!", output[6]);
         stopCapture();
     }
+    
+    @Test
+    public void pressing1AddsReference() {
+        setupTest("1\nBlaa\nBlaa\nBlaa\nBlaa\nBlaa\nBlaa\nBlaa\nBlaa\nBlaa\nBlaa\nBlaa\n4", new Session());
+        startCapture();
+        cli.start();
+        String[] output = out.toString().split("\n");
+        System.out.println(output[0]);
+        assertEquals("Reference added:", output[8]);
+        stopCapture();
+    }
+    
+    @Test
+    public void requiredFieldsMustNotBeEmpty() {
+        setupTest("1\n\nBlaa\nBlaa\nBlaa\nBlaa\nBlaa\nBlaa\nBlaa\nBlaa\nBlaa\nBlaa\nBlaa\n4", new Session());
+        startCapture();
+        cli.start();
+        String[] output = out.toString().split("\n");
+        System.out.println(output[0]);
+        assertEquals("identifier*: Required field!", output[7]);
+        stopCapture();
+    }
 
+    @Test
+    public void invalidInputDoesNothing() {
+        setupTest("u\n4", new Session());
+        startCapture();
+        cli.start();
+        String[] output = out.toString().split("\n");
+        System.out.println(output[0]);
+        assertEquals("> Please input a number.", output[6]);
+        stopCapture();
+    }
+    
+    @Test
+    public void pressing2ShowsPreview() {
+        Session session = getSessionWithArticle();
+        setupTest("2\n4", session);
+        startCapture();
+        cli.start();
+        String[] output = out.toString().split("\n");
+        System.out.println(output[0]);
+        assertEquals("> author: Esko", output[6]);
+        stopCapture();
+    }
+    
+    @Test
+    public void pressing3ShowsBibtexPreview() {
+        Session session = getSessionWithArticle();
+        setupTest("3\n4", session);
+        startCapture();
+        cli.start();
+        String[] output = out.toString().split("\n");
+        System.out.println(output[0]);
+        assertEquals("author = {Esko},", output[7]);
+        stopCapture();
+    }
 }
