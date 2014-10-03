94b129fbb3f094168dca861dd00b8b0827354732
Riku Walve <riku.walve@gmail.com>
2014-10-03 11:17:00 +0300

    inproceedings viitetyyppi lisätty

---
+++ src/main/java/jojoriot/references/Inproceedings.java
@@ -0,0 +1,59 @@
+package jojoriot.references;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * An article in a conference proceedings.
+ */
+public class Inproceedings extends Reference {
+    /**
+     * List of required fields
+     */
+    public static final ArrayList<String> REQUIRED_FIELDS = new ArrayList<>();
+    
+    /**
+     * List of optional fields
+     */
+    public static final ArrayList<String> OPTIONAL_FIELDS = new ArrayList<>();
+    
+    static {
+        REQUIRED_FIELDS.add("author");
+        REQUIRED_FIELDS.add("title");
+        REQUIRED_FIELDS.add("booktitle");
+        REQUIRED_FIELDS.add("year");
+        
+        OPTIONAL_FIELDS.add("editor");
+        OPTIONAL_FIELDS.add("volume/number");
+        OPTIONAL_FIELDS.add("series");
+        OPTIONAL_FIELDS.add("pages");
+        OPTIONAL_FIELDS.add("address");
+        OPTIONAL_FIELDS.add("month");
+        OPTIONAL_FIELDS.add("organization");
+        OPTIONAL_FIELDS.add("publisher");
+        OPTIONAL_FIELDS.add("note");
+        OPTIONAL_FIELDS.add("key");
+    }
+
+    /**
+     *
+     * @param identifier
+     * @param fields
+     */
+    public Inproceedings(final String identifier,
+            final LinkedHashMap<String, String> fields) {
+        super(identifier, REQUIRED_FIELDS, OPTIONAL_FIELDS);
+
+        for (final Map.Entry<String, String> entry : fields.entrySet()) {
+            super.put(entry.getKey(), entry.getValue());
+        }
+        
+        for (String field : REQUIRED_FIELDS) {
+            if (super.get(field) == null) {
+                throw new IllegalArgumentException("Missing required field \""
+                        + field + "\".");
+            }
+        }
+    }
+}
